52af5cdff4d6c01fb9e4dfa356aef642676c3367
Alejandro Novellino <alnovellino07@gmail.com>
2024-09-24 01:33:32 -0400

    update redirect url

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -2,8 +2,6 @@ export const environment = {
   base_url: 'http://localhost:3000/api',
   auth_issuer: 'https://ucab-g1it55.us1.zitadel.cloud',
   auth_client_id: '286268015425705024',
-  auth_redirect_url:
-    'https://election-architect-jkgpsbcnq-alejandros-projects-309155d3.vercel.app',
-  post_logout_redirect_uri:
-    'https://election-architect-jkgpsbcnq-alejandros-projects-309155d3.vercel.app',
+  auth_redirect_url: 'https://election-architect.vercel.app/',
+  post_logout_redirect_uri: 'https://election-architect.vercel.app/',
 };
